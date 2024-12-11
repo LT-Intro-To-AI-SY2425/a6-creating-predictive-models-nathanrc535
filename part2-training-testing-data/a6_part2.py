f64b1bf9bdae1453d2c033c90308aed3d3e452ff
Nathan <nchen6@cps.edu>
2024-12-11 10:40:53 -0600

    Fininshed apart 1 writeup

--- part2-training-testing-data/a6_part2.py
+++ part2-training-testing-data/a6_part2.py
@@ -13,32 +13,63 @@ x = data["Age"].values
 y = data["Blood Pressure"].values
 
 # Create your training and testing datasets:
-
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
+print(x)
+print(xtrain)
+print(y)
+print(ytrain)
+print(ytest)
 # Use reshape to turn the x values into 2D arrays:
-# xtrain = xtrain.reshape(-1,1)
+xtrain = xtrain.reshape(-1,1)
 
 # Create the model
-
+model = LinearRegression().fit(xtrain, ytrain)
 # Find the coefficient, bias, and r squared values. 
 # Each should be a float and rounded to two decimal places. 
-
+coef = round(float(model.coef_[0]), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(xtrain, ytrain)
 
 # Print out the linear equation and r squared value:
+print(f"Linear equation is y= {coef} x + {intercept}" )
+print(f"r squared value is: {r_squared}" )
+
+#TESTING AGE OF 43
+x_predict = 43
+prediction = model.predict([[x_predict]])
+print(f"PREDICTED AT AGE 43 is {prediction}")
 
 '''
 **********TEST THE MODEL**********
 '''
 # reshape the xtest data into a 2D array
-
+xtest = xtest.reshape(-1,1)
 # get the predicted y values for the xtest values - returns an array of the results
-
+predict = model.predict(xtest)
 # round the value in the np array to 2 decimal places
-
+predict = np.around(predict, 2)
 
 # Test the model by looping through all of the values in the xtest dataset
 print("\nTesting Linear Model with Testing Data:")
-
+for index in range(len(xtest)):
+    actual = ytest[index] # gets the actual y value from the ytest dataset
+    predicted_y = predict[index] # gets the predicted y value from the predict variable
+    x_coord = xtest[index] # gets the x value from the xtest dataset
+    print("x value:", float(x_coord[0]), "Predicted y value:", predicted_y, "Actual y value:", actual)
 
 '''
 **********CREATE A VISUAL OF THE RESULTS**********
 '''
+plt.figure(figsize=(5,4))
+
+plt.scatter(xtrain, ytrain , c = "pink", label = "Training Data")
+plt.scatter(xtest, ytest, c = "black", label = "Testing Data")
+plt.scatter(xtest, predict, c = "red", label = "Predictions")
+
+plt.xlabel("Age")
+plt.ylabel("Blood Pressure")
+plt.title("Blood Pressure by Age")
+plt.plot(x, coef*x + intercept, c = "blue", label = "Line of Best Fit")
+
+plt.legend()
+plt.show()
