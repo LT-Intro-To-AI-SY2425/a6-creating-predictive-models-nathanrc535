746d6d5ea66f4680c144e0b2f35db75a59c20e77
Nathan <nchen6@cps.edu>
2024-12-16 10:07:31 -0600

    Finished a6 part3

--- part3-multivariable-linear-regression/a6_part3.py
+++ part3-multivariable-linear-regression/a6_part3.py
@@ -9,14 +9,41 @@ x = data[["miles","age"]].values
 y = data["Price"].values
 
 #split the data into training and testing data
-
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size = .2)
 #create linear regression model
-
+model = LinearRegression().fit(xtrain, ytrain)
 #Find and print the coefficients, intercept, and r squared values. 
 #Each should be rounded to two decimal places. 
-
-
+coef = np.around(model.coef_, 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = round(model.score(x, y),2)
 #Loop through the data and print out the predicted prices and the 
 #actual prices
+print("The Coefficient is:", coef)
+print(f"Model's Linear Equation is y= {coef[0]}x + {coef[1]}x1 + {intercept}")
+print("R Squared Value is:", r_squared)
+
+
 print("***************")
 print("Testing Results")
+
+predict = model.predict(xtest)
+
+predict = np.around(predict, 2)
+print(predict)
+
+xPredictionMile = 89000
+xPredictionAge = 10
+prediction = model.predict([[xPredictionMile, xPredictionAge]])
+print(f"Prediction when miles is {xPredictionMile} and age is {xPredictionAge}: {prediction}")
+
+xPredictionMile2 = 150000
+xPredictionAge2 = 20
+prediction2 = model.predict([[xPredictionMile2, xPredictionAge2]])
+print(f"Prediction when miles is {xPredictionMile2} and age is {xPredictionAge2}: {prediction2}")
+
+print("\nTesting Multivariable Model with Testing Data:")
+for index in range(len(xtest)):
+    actual = ytest[index] # gets the actual y value from the ytest dataset
+    predicted_y = predict[index] # gets the predicted y value from the predict variable
+    x_coord = xtest[index] # gets the x value from the xtest dataset
--- part3-multivariable-linear-regression/a6_part3_writeup.md
+++ part3-multivariable-linear-regression/a6_part3_writeup.md
@@ -5,9 +5,10 @@ After completing `a6_part3.py` answer the following questions
 ## Questions to answer
 
 1. What is the R Squared coefficient for your model? What does that mean about the model in relation to your data?
-
+R squared value is 0.85. This is the best model so far since it is close to 1 showing a positive correlation.
 2. Is your model accurate? Why or why not?
-
+This model is the most accurate and would say is accurate enough due to the r squared value.
 3. What does the model predict a 10-year-old car with 89000 miles is worth? What about a car that is 20 years old with 150000 miles?
-
+My model predicted that the 10 y.o car is worth $9274.67. My model predicted that the 20 y.o car is worth $2299.53
 4. You may notice that some of your predicted results are negative. This is occurring when the value of age and the mileage of the car are very high. Why do you think this is happening?
+Since there was no boundary coded for 0, the model will continue to decrease in value as age and mileage increase. Once the value passes 0, the car will eventually be worth nothing.
